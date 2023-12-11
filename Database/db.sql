94beb588100da7eeec0da33bc82f22bc0d233cd6
shubhanshu1902 <shubhanshu1902@gmail.com>
2023-12-11 15:29:10 +0530

    final testing

--- Database/db.sql
+++ Database/db.sql
@@ -1,10 +1,2 @@
 CREATE DATABASE IF NOT EXISTS UserManagement;
 USE UserManagement;
-
-CREATE TABLE IF NOT EXISTS users(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    status VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id)
-);
